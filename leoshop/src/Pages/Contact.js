46e006095442ce0df7bcc61b27f1f690b2e40232
Lipan Padhan <lipanpadhan1432@gmail.com>
2023-07-31 16:15:50 +0530

    adding contact page

--- leoshop/src/Pages/Contact.js
+++ leoshop/src/Pages/Contact.js
@@ -1,11 +1,86 @@
-import React from 'react'
+import React from "react";
+import { styled } from "styled-components";
 
 const Contact = () => {
-  return (
-    <div>
-      contact
-    </div>
-  )
-}
+  const Wrapper = styled.section`
+    padding: 9rem 0 5rem 0;
+    text-align: center;
 
-export default Contact
+    .container {
+      margin-top: 6rem;
+
+      .contact-form {
+        max-width: 50rem;
+        margin: auto;
+
+        .contact-inputs {
+          display: flex;
+          flex-direction: column;
+          gap: 3rem;
+
+          input[type="submit"] {
+            cursor: pointer;
+            transition: all 0.2s;
+
+            &:hover {
+              background-color: ${({ theme }) => theme.colors.white};
+              border: 1px solid ${({ theme }) => theme.colors.btn};
+              color: ${({ theme }) => theme.colors.btn};
+              transform: scale(0.9);
+            }
+          }
+        }
+      }
+    }
+  `;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact Page</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29858.22124642912!2d83.46358368503552!3d20.698946771451176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a240e97b2bb4bf7%3A0x82d2178de3f7edb7!2sBalangir%2C%20Odisha!5e0!3m2!1sen!2sin!4v1690797680845!5m2!1sen!2sin"
+        width="100%"
+        height="300"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/xvojzknl"
+            method="POST"
+            className="contact-inputs"
+          >
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+            />
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+            <textarea
+              placeholder="Enter your message"
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+            ></textarea>
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Contact;
