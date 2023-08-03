29c2d99190641a906ff7502dc73a68172b6cdcdb
Lipan Padhan <lipanpadhan1432@gmail.com>
2023-08-03 13:10:00 +0530

    adding api call

--- leoshop/src/context/productcontext.js
+++ leoshop/src/context/productcontext.js
@@ -2,11 +2,26 @@
 // provider
 // consumer => useContext Hook
 
-import { createContext, useContext } from "react";
-
+import { createContext, useContext, useEffect } from "react";
+import axios from "axios"
 const AppContext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products";
+
 const AppProvider = ({ children }) => {
+
+
+  const getProducts = async(url) =>{
+        const res = await axios.get(url);
+        // console.log('res', res);
+        const products = await res.data
+        console.log('products', products);
+  }
+
+  useEffect(()=>{
+  getProducts(API);
+  },[])
+
   return (
     <AppContext.Provider value={{ myName: "leo shop" }}>
       {children}
