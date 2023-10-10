7e83c41923d2d7163162a5e970c9e4536fe04ab0
Lipan Padhan <lipanpadhan1432@gmail.com>
2023-10-10 16:53:50 +0530

    adding star rating

--- leoshop/src/Pages/Singleproduct.js
+++ leoshop/src/Pages/Singleproduct.js
@@ -8,6 +8,7 @@ import { Container } from "../styles/Container";
 import FormatPrice from "../Helpers/FormatPrice";
 import { TbTruckDelivery, TbReplace } from "react-icons/tb";
 import { MdSecurity } from "react-icons/md";
+import Star from "../components/Star";
 
 const API = "https://api.pujakaitem.com/api/products";
 
@@ -50,8 +51,7 @@ const Singleproduct = () => {
           {/* product data */}
           <div className="product-data">
             <h2>{name}</h2>
-            <p>{stars}</p>
-            <p>{reviews} reviews</p>
+            <Star stars={stars} reviews={reviews}/>
             <p className="product-data-price">
               MRP:
               <del>
