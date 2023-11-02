c44d36a6f6742f6d2b7a287c48cc425087eff6bd
Lipan Padhan <lipanpadhan1432@gmail.com>
2023-11-02 20:57:41 +0530

    adding price range filter

--- leoshop/src/reducer/filterReducer.js
+++ leoshop/src/reducer/filterReducer.js
@@ -98,7 +98,7 @@ const filterReducer = (state, action) => {
       let { all_products } = state;
       let tempFilterProduct = [...all_products];
 
-      const { text, category, company, color } = state.filters;
+      const { text, category, company, color, price } = state.filters;
 
       if (text) {
         tempFilterProduct = tempFilterProduct.filter((curElem) => {
@@ -118,6 +118,15 @@ const filterReducer = (state, action) => {
       if (color !== "all") {
         tempFilterProduct = tempFilterProduct.filter((curElem) => {
           return curElem.colors.includes(color);
+        });
+      }
+      if (price === 0) {
+        tempFilterProduct = tempFilterProduct.filter((curElem) => {
+          return curElem.price == price;
+        });
+      } else {
+        tempFilterProduct = tempFilterProduct.filter((curElem) => {
+          return curElem.price <= price;
         });
       }
 
